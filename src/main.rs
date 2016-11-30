1a808fc9a46d39028df3096ef62fb5f70e80565e
green4free <ninjalostinsnow@gmail.com>
2016-11-30 11:22:46 +0100

    Added initial code for binary search tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use Tree::*;
+
+enum Tree {
+	Cons(u32, Box<Tree>, Box<Tree>),
+	Nil,
+}
+
+impl Tree {
+	fn new() -> Tree {
+		Nil
+	}
+
+	fn add(self, elem: u32) -> Tree {
+		match self{
+			Nil => Cons(elem, Box::new(Nil), Box::new(Nil)),
+			Cons(data, left, right) => {
+				if elem < data {
+					Cons(data, Box::new(left.add(elem)), right)
+				}else{
+					Cons(data, left, Box::new(right.add(elem)))
+				}
+			},
+		}
+	}
+	
+	fn stringify(&self) -> String {
+		match *self {
+			Nil => format!(""),
+			Cons(data, ref left, ref right) => format!("{} {} {}", left.stringify(), data, right.stringify()),
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+	let mut tree = Tree::new();
+	tree = tree.add(5);
+	tree = tree.add(8);
+	tree = tree.add(3);
+	tree = tree.add(6);
+	tree = tree.add(9);
+	tree = tree.add(11);
+	tree = tree.add(10);
+	tree = tree.add(4);
+	println!("{}", tree.stringify());    
 }
