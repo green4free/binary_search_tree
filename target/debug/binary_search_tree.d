c8a7034fe1006e71dc5ca5cccc0b05d7cfda50d9
green4free <ninjalostinsnow@gmail.com>
2018-03-20 00:14:58 +0100

    checking up on old rust-project

---
+++ target/debug/binary_search_tree.d
@@ -0,0 +1,1 @@
+/home/magnus/Documents/developer/rust/binary_search_tree/target/debug/binary_search_tree: /home/magnus/Documents/developer/rust/binary_search_tree/src/main.rs
---
+++ target/debug/deps/binary_search_tree-a7ed99deb0683caa.d
@@ -0,0 +1,5 @@
+/home/magnus/Documents/developer/rust/binary_search_tree/target/debug/deps/binary_search_tree-a7ed99deb0683caa: src/main.rs
+
+/home/magnus/Documents/developer/rust/binary_search_tree/target/debug/deps/binary_search_tree-a7ed99deb0683caa.d: src/main.rs
+
+src/main.rs:
